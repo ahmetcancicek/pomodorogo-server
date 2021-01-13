f66be0c156bda6fc5a3aaf5fd7877df9d803c336
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-01-13 20:53:25 +0300

    Create model package and Add user model

---
+++ internal/app/model/user.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
+// User ...
+type User struct {
+	ID        int64     `json:"id"`
+	UUID      uuid.UUID `json:"uuid"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
