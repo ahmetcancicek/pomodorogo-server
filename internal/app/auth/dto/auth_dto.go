e0b83a76583f6aa92b67367db87bbae5fd2d4fe3
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-02-10 14:00:01 +0300

    Add struct for auth and token

--- internal/app/auth/dto/auth_dto.go
+++ internal/app/auth/dto/auth_dto.go
@@ -1,5 +1,10 @@
 package dto
 
+import (
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
 type AuthSignUpDTO struct {
 	FirstName string `validate:"required" json:"firstName"`
 	LastName  string `validate:"required" json:"lastName"`
@@ -16,4 +21,13 @@ type AuthLoginDTO struct {
 type AuthLoginResponseDTO struct {
 	AccessToken  string `json:"accessToken"`
 	RefreshToken string `json:"refreshToken"`
+}
+
+type TokenDTO struct {
+	AccessToken             string `json:"accessToken"`
+	RefreshToken            string `json:"refreshToken"`
+	AccessTokenExpiresTime  time.Time
+	RefreshTokenExpiresTime time.Time
+	AccessTokenUUID         uuid.UUID
+	RefreshTokenUUID        uuid.UUID
 }
