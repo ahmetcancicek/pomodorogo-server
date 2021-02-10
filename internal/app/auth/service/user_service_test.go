5150059f06f270d83545289d683a22b3b61a279b
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-02-10 14:27:42 +0300

    Create unit test for user service

---
+++ internal/app/auth/service/user_service_test.go
@@ -0,0 +1,19 @@
+package service_test
+
+import "testing"
+
+func FindByID(t *testing.T) {
+
+}
+
+func Update(t *testing.T) {
+
+}
+
+func Save(t *testing.T) {
+
+}
+
+func Delete(t *testing.T) {
+
+}
