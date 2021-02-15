535e0be75efec7870e65c6a88c38bc0aea1c1cff
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-02-15 15:56:12 +0300

    Update dto for auth

--- internal/app/auth/dto/auth_dto.go
+++ internal/app/auth/dto/auth_dto.go
@@ -1,10 +1,5 @@
 package dto
 
-import (
-	uuid "github.com/satori/go.uuid"
-	"time"
-)
-
 type AuthSignUpDTO struct {
 	FirstName string `validate:"required" json:"firstName"`
 	LastName  string `validate:"required" json:"lastName"`
@@ -13,21 +8,12 @@ type AuthSignUpDTO struct {
 	Password  string `validate:"required" json:"password"`
 }
 
-type AuthLoginDTO struct {
+type AuthSignInDTO struct {
 	Email    string `validate:"required" json:"email"`
 	Password string `validate:"required" json:"password"`
 }
 
-type AuthLoginResponseDTO struct {
+type AuthSignInResponseDTO struct {
 	AccessToken  string `json:"accessToken"`
 	RefreshToken string `json:"refreshToken"`
-}
-
-type TokenDTO struct {
-	AccessToken             string `json:"accessToken"`
-	RefreshToken            string `json:"refreshToken"`
-	AccessTokenExpiresTime  time.Time
-	RefreshTokenExpiresTime time.Time
-	AccessTokenUUID         uuid.UUID
-	RefreshTokenUUID        uuid.UUID
 }
