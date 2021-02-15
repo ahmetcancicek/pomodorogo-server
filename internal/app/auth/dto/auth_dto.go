6de315d3cef77a82b942e2d428e23f307beb9dfe
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-02-15 18:44:23 +0300

    Add validate configuration

--- internal/app/auth/dto/auth_dto.go
+++ internal/app/auth/dto/auth_dto.go
@@ -1,16 +1,16 @@
 package dto
 
 type AuthSignUpDTO struct {
-	FirstName string `validate:"required" json:"firstName"`
-	LastName  string `validate:"required" json:"lastName"`
-	Username  string `validate:"required" json:"username"`
-	Email     string `validate:"required" json:"email"`
-	Password  string `validate:"required" json:"password"`
+	FirstName string `validate:"required,max=50,min=3" json:"firstName"`
+	LastName  string `validate:"required,max=50,min=3" json:"lastName"`
+	Username  string `validate:"required,max=50,min=3" json:"username"`
+	Email     string `validate:"required,max=75,min=3" json:"email"`
+	Password  string `validate:"required,max=50,min=3" json:"password"`
 }
 
 type AuthSignInDTO struct {
-	Email    string `validate:"required" json:"email"`
-	Password string `validate:"required" json:"password"`
+	Email    string `validate:"required,max=75,min=3" json:"email"`
+	Password string `validate:"required,max=50,min=3" json:"password"`
 }
 
 type AuthSignInResponseDTO struct {
