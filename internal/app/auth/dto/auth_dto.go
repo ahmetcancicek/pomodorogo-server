7dc1cd0adbd0bb45069c87ffeba7c7f0534aaccf
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-02-17 22:22:48 +0300

    Update Authentication DTO

--- internal/app/auth/dto/auth_dto.go
+++ internal/app/auth/dto/auth_dto.go
@@ -1,18 +1,5 @@
 package dto
 
-type AuthSignUpDTO struct {
-	FirstName string `validate:"required,max=50,min=3" json:"firstName"`
-	LastName  string `validate:"required,max=50,min=3" json:"lastName"`
-	Username  string `validate:"required,max=50,min=3" json:"username"`
-	Email     string `validate:"required,max=75,min=3" json:"email"`
-	Password  string `validate:"required,max=50,min=3" json:"password"`
-}
-
-type AuthSignInDTO struct {
-	Email    string `validate:"required,max=75,min=3" json:"email"`
-	Password string `validate:"required,max=50,min=3" json:"password"`
-}
-
 type AuthSignInResponseDTO struct {
 	AccessToken  string `json:"accessToken"`
 	RefreshToken string `json:"refreshToken"`
