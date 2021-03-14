e572cab9c7f543df0c2f09352f861d1520583a73
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-03-14 10:35:31 +0300

    Update error model

--- internal/app/model/error.go
+++ internal/app/model/error.go
@@ -8,11 +8,13 @@ import (
 var (
 	ErrNotFound = errors.New("your requested item is not found")
 
-	ErrUserSignInFailed          = fmt.Sprint("No user account exists with given email or password.")
+	ErrUserSignInFailed          = fmt.Sprint("No user account exists with given email or password")
 	ErrUserSignUpFailed          = fmt.Sprintf("Unable to create user. Please try again later")
 	ErrUserEmailAlreadyExists    = fmt.Sprintf("User already exists with the given email")
 	ErrUserUsernameAlreadyExists = fmt.Sprintf("User already exists with the given username")
 
 	ErrTagCreateFailed  = fmt.Sprintf("Unable to tag user. Please try again later")
 	ErrTagAlreadyExists = fmt.Sprintf("Tag already exists with the given name")
+
+	ErrStatisticTagDoesNotExists = fmt.Sprintf("No tag exists for statistic")
 )
