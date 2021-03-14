5ea6124ffa6f2b4278e31aa5e11ad9936b6ab42d
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-03-14 12:04:41 +0300

    Implementation service

--- internal/app/statistic/service.go
+++ internal/app/statistic/service.go
@@ -4,5 +4,5 @@ import "github.com/ahmetcancicek/pomodorogo-server/internal/app/statistic/dto"
 
 // StatisticService represent the statistic's service
 type Service interface {
-	Save(statDTO *dto.StatisticDTO) (*dto.StatisticDTO, error)
+	Save(statDTO *dto.StatisticDTO, userId uint) (*dto.StatisticDTO, error)
 }
--- internal/app/statistic/service/statistic_service.go
+++ internal/app/statistic/service/statistic_service.go
@@ -1,28 +1,51 @@
 package service
 
 import (
+	"errors"
+	"github.com/ahmetcancicek/pomodorogo-server/internal/app/model"
 	"github.com/ahmetcancicek/pomodorogo-server/internal/app/statistic"
 	"github.com/ahmetcancicek/pomodorogo-server/internal/app/statistic/dto"
+	"github.com/ahmetcancicek/pomodorogo-server/internal/app/tag"
+	"github.com/ahmetcancicek/pomodorogo-server/internal/app/utils"
+	"github.com/go-playground/validator/v10"
 )
 
 type statisticService struct {
 	statisticRepository statistic.Repository
+	tagRepository       tag.Repository
 }
 
-func NewStatisticService(sr statistic.Repository) statistic.Service {
+func NewStatisticService(statRepo statistic.Repository, tagRepo tag.Repository) statistic.Service {
 	return &statisticService{
-		statisticRepository: sr,
+		statisticRepository: statRepo,
+		tagRepository:       tagRepo,
 	}
 }
 
-func (s statisticService) Save(statDTO *dto.StatisticDTO) (*dto.StatisticDTO, error) {
-	// TODO:
-	// Should validate request data
-	// Checks if user have got this label
-	//
+func (s statisticService) validate(statDTO *dto.StatisticDTO) error {
+	err := utils.PayloadValidator(statDTO)
+	if err != nil {
+		return errors.New(err.(validator.ValidationErrors).Error())
+	}
+	return nil
+}
 
+func (s statisticService) Save(statDTO *dto.StatisticDTO, userId uint) (*dto.StatisticDTO, error) {
+	// 1. Validate request data
+	err := s.validate(statDTO)
+	if err != nil {
+		return nil, err
+	}
+
+	// 2. Checks if user have got his tag
+	_, err = s.tagRepository.FindByIDAndUser(statDTO.TagID, userId)
+	if err != nil {
+		return nil, errors.New(model.ErrStatisticTagDoesNotExists)
+	}
+
+	// 3. Save
 	stat := dto.ToStatistic(statDTO)
-	stat, err := s.statisticRepository.Save(stat)
+	stat, err = s.statisticRepository.Save(stat)
 
 	return dto.ToStatisticDTO(stat), err
 }
